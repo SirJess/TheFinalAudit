2937db49b17304844f592c105b8763cdfa79d220
WinterCherri <decaturhoangyen@gmail.com>
2025-02-08 23:53:41 -0500

    levels and title_screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import EscapeRoom1 from "./components/views/EscapeRoom1";
 import EscapeRoom2 from "./components/views/EscapeRoom2";
+import Title_screen from "./components/views/Title_screen";
+import Levels from "./components/Levels";
+import AllLevels from "./components/AllLevels";
 
 function App() {
   return (
@@ -10,7 +13,11 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Login />} /> */}
         {/* <Route path="/" element={<EscapeRoom1 />} /> */}
-        <Route path="/" element={<EscapeRoom2 />} />
+        {/* <Route path="/" element={<EscapeRoom2 />} /> */}
+        <Route path="/" element={<Title_screen />} />
+        <Route path="/levels" element={<AllLevels />} />
+        <Route path="/escape-room-1" element={<EscapeRoom1 />} />
+        <Route path="/escape-room-2" element={<EscapeRoom2 />} />
       </Routes>
     </Router>
   );
